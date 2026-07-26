1f6a0d82eaa8d28dd480e880ae745b002a0e7bc9
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-07-26 19:08:40 +0200

    create DI extension methods for auto registration

---
+++ src/Shared/src/Shared.AspNetCore.Helpers/Options/DependencyInjection.cs
@@ -0,0 +1,50 @@
+using System.Reflection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.AspNetCore.Helpers.Options.Exceptions;
+
+namespace Shared.AspNetCore.Helpers.Options
+{
+    public static class DependencyInjection
+    {
+        public static void AddOptionsFromAssembly(this IHostApplicationBuilder builder, Assembly assembly)
+        {
+            var options = assembly.GetTypes()
+                .Where(t => t.IsAssignableTo(typeof(IOptions)))
+                .ToList();
+            
+            RegisterOptions(builder, options);
+        }
+
+        public static void AddOptionsFromAssemblies(this IHostApplicationBuilder builder, params Assembly[] assemblies)
+        {
+            var options = new List<Type>();
+            foreach (var assembly in assemblies)
+            {
+                options.AddRange(assembly.GetTypes()
+                    .Where(t => t.IsAssignableTo(typeof(IOptions)))
+                    .ToList());
+            }
+
+            RegisterOptions(builder, options);
+        }
+
+        private static void RegisterOptions(IHostApplicationBuilder builder, List<Type> options)
+        {
+            // builder.Services.Configure<MyOptionsClass>(builder.Configuration.GetSection(MyOptionsClass.Key));
+
+            foreach (var type in options)
+            {
+                var configureMethod = typeof(OptionsConfigurationServiceCollectionExtensions).GetMethods()
+                    .FirstOrDefault(m => m.Name == nameof(OptionsConfigurationServiceCollectionExtensions.Configure))!
+                    .MakeGenericMethod(type);
+
+                var key = (string?)type.GetProperty(nameof(IOptions.Key), BindingFlags.Public | BindingFlags.Static)!.GetValue(null);
+                if (key == null)
+                    throw new OptionsKeyIsNullException(type.Name);
+
+                configureMethod.Invoke(null, [builder.Services, builder.Configuration.GetSection(key)]);
+            }
+        }
+    }
+}
