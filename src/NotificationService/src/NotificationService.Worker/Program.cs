d6950ced91021de86086b63bc8150620a7c0b220
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-08-04 18:59:33 +0200

    register rabbitmq conn service and email worker

--- src/NotificationService/src/NotificationService.Worker/Program.cs
+++ src/NotificationService/src/NotificationService.Worker/Program.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using NotificationService.Worker.Abstractions;
 using NotificationService.Worker.Services;
+using NotificationService.Workers;
 using Shared.AspNetCore.Helpers.Options;
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -8,6 +9,10 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.AddOptionsFromAssembly(Assembly.GetExecutingAssembly());
 
 builder.Services.AddSingleton<IEmailService, SmtpService>();
+
+builder.Services.AddSingleton<RabbitMqConnectionService>();
+
+builder.Services.AddHostedService<EmailWorker>();
 
 var host = builder.Build();
 host.Run();
