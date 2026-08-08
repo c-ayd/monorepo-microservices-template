c2030ef3d870d80b00789dfcfde04a7008310e7a
Cemal A. Aydeniz <201085822+c-ayd@users.noreply.github.com>
2026-08-08 16:13:10 +0200

    feat(api-gateway, auth): add logging middleware (#22)
    The logging middleware from Common.Logging is added to both API Gateway and Auth Service.

--- src/ApiGateway/src/ApiGateway.Web/Program.cs
+++ src/ApiGateway/src/ApiGateway.Web/Program.cs
@@ -1,9 +1,17 @@
+using Common.Logging.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Logging.AddStructuredConsoleLogging(
+    builder.Environment.ApplicationName,
+    builder.Environment.IsDevelopment());
+
 var app = builder.Build();
+
+app.UseLoggingMiddleware();
 
 app.MapReverseProxy();
 
