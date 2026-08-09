338c07c86c30cc1fa5b3225263501ae2d6dc7732
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-08-09 14:43:34 +0200

    create test cases for global exception handler

---
+++ src/AuthService/test/integration/AuthService.Test.Integration.Api/Middlewares/GlobalExceptionHandlerTest.cs
@@ -0,0 +1,53 @@
+using System.Net;
+using System.Net.Http.Json;
+using System.Reflection;
+using AuthService.Api.Middlewares;
+using AuthService.Test.Integration.Api.Collections;
+using AuthService.Test.Utility.Fixtures;
+using Common.Http.Response.Structures;
+
+namespace AuthService.Test.Integration.Api.Middlewares
+{
+    [Collection(nameof(AuthApiCollection))]
+    public class GlobalExceptionHandlerTest
+    {
+        private readonly HttpClient _client;
+
+        public GlobalExceptionHandlerTest(AuthApiFixture authApiFixture)
+        {
+            _client = authApiFixture.Client;
+        }
+
+        [Fact]
+        public async Task Invoke_WhenExceptionIsThrown_ShouldReturn500AndCorrectFormat()
+        {
+            // Act
+            var response = await _client.GetAsync("/test/exception");
+
+            // Assert
+            Assert.NotNull(response);
+
+            var jsonResponse = await response.Content.ReadFromJsonAsync<ErrorResponse>();
+            Assert.NotNull(jsonResponse);
+            Assert.Single(jsonResponse.Errors);
+
+            var internalServerError = typeof(GlobalExceptionHandler).GetField("_internalServerError", BindingFlags.NonPublic | BindingFlags.Static);
+            Assert.Equal(internalServerError!.GetValue(null), jsonResponse.Errors[0]);
+        }
+
+        [Fact]
+        public async Task Invoke_WhenExceptionIsNotThrown_ShouldContinueExecution()
+        {
+            // Act
+            var response = await _client.GetAsync("/test/no-exception");
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+        }
+        private class ErrorResponse
+        {
+            public List<ErrorItem> Errors { get; set; } = new List<ErrorItem>();
+        }
+    }
+}
