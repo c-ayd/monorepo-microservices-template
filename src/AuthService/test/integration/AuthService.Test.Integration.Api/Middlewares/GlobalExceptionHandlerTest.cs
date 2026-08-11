0b53c4c2c293c6797532895d88f7931cd9e7331f
Cemal A. Aydeniz <201085822+c-ayd@users.noreply.github.com>
2026-08-11 22:00:16 +0200

    test(auth): cleanup webapp fixture and global exception test (#33)
    - The extra endpoints in the Web API fixture are refactored
    - A status code check is added to a test case where the global exception middlewar returns 500

--- src/AuthService/test/integration/AuthService.Test.Integration.Api/Middlewares/GlobalExceptionHandlerTest.cs
+++ src/AuthService/test/integration/AuthService.Test.Integration.Api/Middlewares/GlobalExceptionHandlerTest.cs
@@ -26,6 +26,7 @@ namespace AuthService.Test.Integration.Api.Middlewares
 
             // Assert
             Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
 
             var jsonResponse = await response.Content.ReadFromJsonAsync<ErrorResponse>();
             Assert.NotNull(jsonResponse);
