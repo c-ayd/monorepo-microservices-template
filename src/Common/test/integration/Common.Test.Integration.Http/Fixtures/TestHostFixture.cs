77cb5ce3340614c8fd75fe664beb751da517ea63
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-08-11 20:21:05 +0200

    create test host fixture

---
+++ src/Common/test/integration/Common.Test.Integration.Http/Fixtures/TestHostFixture.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Common.Test.Integration.Http.Fixtures
+{
+    public class TestHostFixture : IAsyncLifetime
+    {
+        public IHost? TestHost { get; private set; }
+        public HttpClient? Client { get; private set; }
+
+        public async Task InitializeAsync()
+        {
+        }
+
+        public async Task BuildAsync(Action<IServiceCollection>? configureServices, Action<IApplicationBuilder>? configureApp)
+        {
+            TestHost = Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(config =>
+                {
+                    config.UseTestServer();
+                    config.ConfigureServices(services =>
+                    {
+                        configureServices?.Invoke(services);
+                    });
+                    config.Configure(app =>
+                    {
+                        configureApp?.Invoke(app);
+                    });
+                })
+                .Build();
+            
+            await TestHost.StartAsync();
+
+            Client = TestHost.GetTestClient();
+        }
+
+        public async Task DisposeAsync()
+        {
+            Client?.Dispose();
+
+            if (TestHost != null)
+            {
+                await TestHost.StopAsync();
+                TestHost?.Dispose();
+            }
+        }
+    }
+}
