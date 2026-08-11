aa10178573adad5966450985879820a4d89738ac
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-08-11 20:15:20 +0200

    remove methods accepting multiple validations
    - Changed where the validators are fetched (via DI container now)
    - Removed the not found custom exceptions for validators since the request throw the proper exception

--- src/Common/src/Common.Http/DependencyInjection/ValidationFilters.cs
+++ src/Common/src/Common.Http/DependencyInjection/ValidationFilters.cs
@@ -1,5 +1,4 @@
 using System.Net;
-using Common.Http.Exceptions;
 using Common.Http.Response;
 using Common.Http.Response.Structures;
 using Common.Http.Validation;
@@ -17,9 +16,9 @@ namespace Common.Http.DependencyInjection
         );
 
         /// <summary>
-        /// Adds a validation filter to the endpoint.
+        /// Adds a validator to the endpoint.
         /// </summary>
-        /// <param name="validator">Validator to run against the request</param>
+        /// <typeparam name="T">Type of the value to validate</typeparam>
         public static IEndpointConventionBuilder AddValidation<T>(this IEndpointConventionBuilder builder)
         {
             return builder.AddEndpointFilter(async (context, next) =>
@@ -28,41 +27,10 @@ namespace Common.Http.DependencyInjection
                 if (request == null)
                     return JsonResponseBuilder.Error(HttpStatusCode.BadRequest, [_requestBodyMissingError]);
 
-                var validator = context.HttpContext.RequestServices.GetRequiredService<IValidator<T>>();
-                if (validator == null)
-                    throw new ValidatorNotFoundException($"{nameof(IValidator<>)}<{typeof(T).Name}>");
-
-                var errors = validator.Validate(request);
-                if (errors.Count > 0)
-                    return JsonResponseBuilder.Error(HttpStatusCode.BadRequest, errors);
-
-                return await next(context);
-            });
-        }
-
-        /// <summary>
-        /// Adds validation filters to the endpoint.
-        /// </summary>
-        /// <param name="validators">Validators to run against the request</param>
-        /// <param name="stopOnFirstFail">Whether to stop the validation if a validator returns errors</param>
-        public static IEndpointConventionBuilder AddValidations<T>(this IEndpointConventionBuilder builder,
-            IEnumerable<IValidator<T>> validators,
-            bool stopOnFirstFail = true)
-        {
-            return builder.AddEndpointFilter(async (context, next) =>
-            {
-                var request = context.Arguments.OfType<T>().FirstOrDefault();
-                if (request == null)
-                    return JsonResponseBuilder.Error(HttpStatusCode.BadRequest, [_requestBodyMissingError]);
-
-                var errors = new List<ErrorItem>();
-                foreach (var validator in validators)
-                {
-                    errors.AddRange(validator.Validate(request));
-                    if (stopOnFirstFail && errors.Count > 0)
-                        return JsonResponseBuilder.Error(HttpStatusCode.BadRequest, errors);
-                }
-
+                var errors = context.HttpContext.RequestServices
+                    .GetRequiredService<IValidator<T>>()
+                    .Validate(request);
+                
                 if (errors.Count > 0)
                     return JsonResponseBuilder.Error(HttpStatusCode.BadRequest, errors);
 
@@ -73,7 +41,7 @@ namespace Common.Http.DependencyInjection
         /// <summary>
         /// Adds an async validator to the endpoint.
         /// </summary>
-        /// <param name="validator">Async validator to run against the request</param>
+        /// <typeparam name="T">Type of the value to validate</typeparam>
         public static IEndpointConventionBuilder AddAsyncValidation<T>(this IEndpointConventionBuilder builder)
         {
             return builder.AddEndpointFilter(async (context, next) =>
@@ -82,41 +50,10 @@ namespace Common.Http.DependencyInjection
                 if (request == null)
                     return JsonResponseBuilder.Error(HttpStatusCode.BadRequest, [_requestBodyMissingError]);
 
-                var validator = context.HttpContext.RequestServices.GetRequiredService<IAsyncValidator<T>>();
-                if (validator == null)
-                    throw new AsyncValidatorNotFoundException($"{nameof(IAsyncValidator<>)}<{typeof(T).Name}>");
-
-                var errors = await validator.ValidateAsync(request, context.HttpContext.RequestAborted);
-                if (errors.Count > 0)
-                    return JsonResponseBuilder.Error(HttpStatusCode.BadRequest, errors);
-
-                return await next(context);
-            });
-        }
-
-        /// <summary>
-        /// Adds async validators to the endpoints.
-        /// </summary>
-        /// <param name="validators">Async validator to run against the request</param>
-        /// <param name="stopOnFirstFail">Whether to stop the validation if a validator returns errors</param>
-        public static IEndpointConventionBuilder AddAsyncValidations<T>(this IEndpointConventionBuilder builder,
-            IEnumerable<IAsyncValidator<T>> validators,
-            bool stopOnFirstFail = true)
-        {
-            return builder.AddEndpointFilter(async (context, next) =>
-            {
-                var request = context.Arguments.OfType<T>().FirstOrDefault();
-                if (request == null)
-                    return JsonResponseBuilder.Error(HttpStatusCode.BadRequest, [_requestBodyMissingError]);
-
-                var errors = new List<ErrorItem>();
-                foreach (var validator in validators)
-                {
-                    errors.AddRange(await validator.ValidateAsync(request));
-                    if (stopOnFirstFail && errors.Count > 0)
-                        return JsonResponseBuilder.Error(HttpStatusCode.BadRequest, errors);
-                }
-
+                var errors = await context.HttpContext.RequestServices
+                    .GetRequiredService<IAsyncValidator<T>>()
+                    .ValidateAsync(request, context.HttpContext.RequestAborted);
+                
                 if (errors.Count > 0)
                     return JsonResponseBuilder.Error(HttpStatusCode.BadRequest, errors);
 
