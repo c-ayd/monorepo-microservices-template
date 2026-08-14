ace076ae995368b309c05fa0a9b5b2a7e3299046
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-08-14 21:46:45 +0200

    register dbcontext in DI container

--- src/NotificationService/src/NotificationService.Worker/Program.cs
+++ src/NotificationService/src/NotificationService.Worker/Program.cs
@@ -1,12 +1,17 @@
 using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 using NotificationService.Worker.Abstractions;
+using NotificationService.Worker.DbContexts;
+using NotificationService.Worker.Options;
 using NotificationService.Worker.Services;
-using NotificationService.Workers;
 using Shared.AspNetCore.Helpers.DependencyInjection;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.AddOptionsFromAssembly(Assembly.GetExecutingAssembly());
+
+var connStrings = builder.Configuration.GetSection(ConnectionStringsOptions.Key).Get<ConnectionStringsOptions>()!;
+builder.Services.AddDbContext<TemplateDbContext>(_ => _.UseNpgsql(connStrings.TemplateDb));
 
 builder.Services.AddSingleton<IEmailService, SmtpService>();
 
