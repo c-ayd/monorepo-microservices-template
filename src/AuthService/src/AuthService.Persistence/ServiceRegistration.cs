ee76e4d5ff9e060ddabc28c465251bd5439f2003
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-08-20 11:58:45 +0200

    refactor getting conn string from config

--- src/AuthService/src/AuthService.Persistence/ServiceRegistration.cs
+++ src/AuthService/src/AuthService.Persistence/ServiceRegistration.cs
@@ -11,9 +11,8 @@ namespace AuthService.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
-            var connStrings = configuration.GetSection(ConnectionStringsOptions.Key).Get<ConnectionStringsOptions>()!;
-
-            services.AddDbContext<AuthDbContext>(_ => _.UseNpgsql(connStrings.AuthDb));
+            services.AddDbContext<AuthDbContext>(_ => 
+                _.UseNpgsql(configuration.GetConnectionString(nameof(ConnectionStringsOptions.AuthDb))));
             services.AddScoped<IAuthDbContext>(sp => sp.GetRequiredService<AuthDbContext>());
         }
     }
