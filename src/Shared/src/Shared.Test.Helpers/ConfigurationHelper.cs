6f7739c98fe7e88de18f696e411377674607d8f3
Cemal A. Aydeniz <201085822+c-ayd@users.noreply.github.com>
2026-08-25 19:12:13 +0200

    feat(shared): create shared library for test helpers (#63)
    Currently, a configuration helper is added to create configuration from a given JSON file.

---
+++ src/Shared/src/Shared.Test.Helpers/ConfigurationHelper.cs
@@ -0,0 +1,32 @@
+using Microsoft.Extensions.Configuration;
+
+namespace Shared.Test.Helpers
+{
+    /// <summary>
+    /// Provides methods to create configuration from JSON files
+    /// </summary>
+    public static class ConfigurationHelper
+    {
+        /// <summary>
+        /// Creates configuration from 'appsettings.Test.json' file.
+        /// </summary>
+        /// <returns>Returns created configuration file.</returns>
+        public static IConfiguration CreateConfigurationFromTestSettings()
+        {
+            return CreateConfigurationFromFile("appsettings.Test.json");
+        }
+
+        /// <summary>
+        /// Creates configuration from a given JSON file.
+        /// </summary>
+        /// <param name="fileName">Name of the JSON file</param>
+        /// <returns>Returns created configuration file.</returns>
+        public static IConfiguration CreateConfigurationFromFile(string fileName)
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile(fileName)
+                .Build();
+        }
+    }
+}
