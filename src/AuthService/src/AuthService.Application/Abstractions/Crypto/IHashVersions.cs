1c64a56a5f013e7e83c843a924afac0916e76a51
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-08-26 15:36:20 +0200

    add xml doc to hash versions

--- src/AuthService/src/AuthService.Application/Abstractions/Crypto/IHashVersions.cs
+++ src/AuthService/src/AuthService.Application/Abstractions/Crypto/IHashVersions.cs
@@ -2,11 +2,23 @@ using Shared.Crypto.Options;
 
 namespace AuthService.Application.Abstractions.Crypto
 {
+    /// <summary>
+    /// Provides methods to get available hash versions.
+    /// </summary>
     public interface IHashVersions
     {
         byte CurrentHashVersion { get; }
 
+        /// <summary>
+        /// Gets the hash options based on <see cref="IHashVersions.CurrentHashVersion"/>.
+        /// </summary>
+        /// <returns>Returns the current hash options.</returns>
         HashOptions GetCurrentHashOption();
+        /// <summary>
+        /// Gets the hash options based on a given version.
+        /// </summary>
+        /// <param name="version">Version of the hash options.</param>
+        /// <returns>Returns the hash options.</returns>
         HashOptions GetHashOptions(byte version);
     }
 }
