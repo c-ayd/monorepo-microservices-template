e80a1166db882e27fa062dcdf518ffe15658f501
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-08-27 14:07:25 +0200

    create test cases for account preferences middleware

---
+++ src/AuthService/test/unit/AuthService.Test.Unit.Api/Middlewares/AccountPreferenceMiddlewareTest.cs
@@ -0,0 +1,44 @@
+using AuthService.Api.Middlewares;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+namespace AuthService.Test.Unit.Api.Middlewares
+{
+    public class AccountPreferenceMiddlewareTest
+    {
+        private readonly AccountPreferenceMiddleware _middleware;
+
+        public AccountPreferenceMiddlewareTest()
+        {
+            _middleware = new AccountPreferenceMiddleware(async (context) => {});
+        }
+
+        [Fact]
+        public async Task Invoke_WhenUserPreferencesExist_ShouldAddPreferencesToItems()
+        {
+            // Arrange
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers.Append(HeaderNames.AcceptLanguage, "en-US;q=0.9,de-DE;q=0.8,en;q=0.7");
+
+            // Act
+            await _middleware.Invoke(httpContext);
+
+            // Assert
+            Assert.NotNull(httpContext.Items["PreferredLanguage"]);
+            Assert.Equal("en", httpContext.Items["PreferredLanguage"]!.ToString());
+        }
+
+        [Fact]
+        public async Task Invoke_WhenUserPreferencesDoNotExist_ShouldAddNothingToItems()
+        {
+            // Arrange
+            var httpContext = new DefaultHttpContext();
+
+            // Act
+            await _middleware.Invoke(httpContext);
+
+            // Assert
+            Assert.Null(httpContext.Items["PreferredLanguage"]);
+        }
+    }
+}
