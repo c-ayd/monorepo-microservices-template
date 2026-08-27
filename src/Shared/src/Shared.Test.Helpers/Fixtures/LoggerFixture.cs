e28138c59b83816ca9b6293dfd8a404aaab33a44
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-08-27 18:24:57 +0200

    create logger fixture

---
+++ src/Shared/src/Shared.Test.Helpers/Fixtures/LoggerFixture.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.Logging;
+
+namespace Shared.Test.Helpers.Fixtures
+{
+    public class LoggerFixture<T> : ILogger<T>
+    {
+        public IDisposable? BeginScope<TState>(TState state)
+            where TState : notnull
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+        }
+    }
+}
