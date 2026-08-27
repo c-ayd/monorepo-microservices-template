398683bab3f3ed2ed1e95acce18b12f9493cfc34
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-08-27 18:36:42 +0200

    remove integration test of api gateway since there is no test cases

--- src/ApiGateway/test/integration/ApiGateway.Test.Integration.Web/Collections/ApiGatewayWebCollection.cs
+++
@@ -1,9 +0,0 @@
-using ApiGateway.Test.Integration.Web.Fixtures;
-
-namespace ApiGateway.Test.Integration.Web.Collections
-{
-    [CollectionDefinition(nameof(ApiGatewayWebCollection))]
-    public class ApiGatewayWebCollection : ICollectionFixture<ApiGatewayWebFixture>
-    {
-    }
-}
