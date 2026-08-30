cf4cfbea8c0946b16c1d3c47f89ee7ac98f37d0b
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-08-30 15:03:02 +0200

    add session id to cookies

--- src/Shared/src/Shared.Http/Authentication/CookieKeys.cs
+++ src/Shared/src/Shared.Http/Authentication/CookieKeys.cs
@@ -2,6 +2,7 @@ namespace Shared.Http.Authentication
 {
     public static class CookieKeys
     {
+        public const string SessionId = "session_id";
         public const string RefreshToken = "refresh_token";
     }
 }
