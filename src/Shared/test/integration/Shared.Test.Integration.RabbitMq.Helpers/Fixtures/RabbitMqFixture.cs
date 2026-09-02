21c1bd5b8c6a0bc2064637af7d6da01219a3f59a
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-09-02 20:04:16 +0200

    create rabbitmq fixture for rabbitmq helpers test project

---
+++ src/Shared/test/integration/Shared.Test.Integration.RabbitMq.Helpers/Fixtures/RabbitMqFixture.cs
@@ -0,0 +1,41 @@
+using DotNet.Testcontainers.Builders;
+using RabbitMQ.Client;
+using Testcontainers.RabbitMq;
+
+namespace Shared.Test.Integration.RabbitMq.Helpers.Fixtures
+{
+    public class RabbitMqFixture : IAsyncLifetime
+    {
+        private RabbitMqContainer _container = null!;
+
+        public IConnection Connection { get; private set; } = null!;
+
+        public async Task InitializeAsync()
+        {
+            _container = new RabbitMqBuilder("rabbitmq:4.3.4-management")
+                .WithPortBinding(5672, true)
+                .WithPortBinding(15672, true)
+                .WithEnvironment("RABBITMQ_DEFAULT_USER", "guest")
+                .WithEnvironment("RABBITMQ_DEFAULT_PASS", "guest")
+                .WithWaitStrategy(Wait.ForUnixContainer().UntilMessageIsLogged(".*Server startup complete.*"))
+                .Build();
+            await _container.StartAsync();
+
+            var factory = new ConnectionFactory()
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost",
+                Port = _container.GetMappedPublicPort(5672)
+            };
+
+            Connection = await factory.CreateConnectionAsync();
+        }
+
+        public async Task DisposeAsync()
+        {
+            await _container.StopAsync();
+            await _container.DisposeAsync();
+        }
+    }
+}
