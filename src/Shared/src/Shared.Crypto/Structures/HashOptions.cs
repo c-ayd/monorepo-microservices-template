daaa0ff19b0b4179f04014b65d66f309264331a7
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-09-04 01:12:24 +0200

    add xml code doc to hash options

--- src/Shared/src/Shared.Crypto/Structures/HashOptions.cs
+++ src/Shared/src/Shared.Crypto/Structures/HashOptions.cs
@@ -2,6 +2,11 @@ using System.Security.Cryptography;
 
 namespace Shared.Crypto.Structures
 {
+    /// <summary>
+    /// Represents hashing options that are used in <see cref="ValueHasher"/>.
+    /// </summary>
+    /// <param name="Algorithm">Type of the hashing algorithm</param>
+    /// <param name="SaltSize">Size of the salt</param>
     public record HashOptions(
         Func<HashAlgorithm> Algorithm,
         int SaltSize
