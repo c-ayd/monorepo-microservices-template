a74d2b3b5c97522a48d5736a67b72bc93dacddb9
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-09-12 18:12:28 +0200

    create auth db initializer
    The initializer takes the responsibility of migration and seed data

--- src/InitializerServices/src/AuthDb.Initializer/Program.cs
+++ src/InitializerServices/src/AuthDb.Initializer/Program.cs
@@ -1,1 +1,135 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using AuthDb.Initializer.Exceptions;
+using AuthDb.Initializer.Options;
+using AuthService.Domain.Entities;
+using AuthService.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Shared.Constants;
+
+namespace AuthDb.Initializer
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            System.Console.WriteLine("AuthDB Initializer started.");
+
+            bool force = args.Contains("--force") || args.Contains("-f");  // If true, it will try to seed data whether there is data or not in the DB.
+
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+            var seedDataOptions = configuration.GetSection("SeedData").Get<AuthDbSeedDataOptions>()!;
+
+            using var authDbContext = new AuthDbContext(new DbContextOptionsBuilder<AuthDbContext>()
+                .UseNpgsql(configuration.GetConnectionString("AuthDb"))
+                .Options);
+
+            await InitializeAsync(force, seedDataOptions, authDbContext);
+
+            return 0;
+        }
+
+        private static async Task InitializeAsync(bool force, AuthDbSeedDataOptions seedDataOptions, AuthDbContext authDbContext)
+        {
+            // Migrate
+            await authDbContext.Database.MigrateAsync();
+
+            System.Console.WriteLine("Migration completed.");
+
+            // Seed data
+            await using var transaction = await authDbContext.Database.BeginTransactionAsync();
+            try
+            {
+                await SeedRolesAsync(force, seedDataOptions, authDbContext);
+                await SeedAccountsAsync(force, seedDataOptions, authDbContext);
+
+                await transaction.CommitAsync();
+
+                System.Console.WriteLine("Seed data is committed to the DB.");
+            }
+            catch (Exception)
+            {
+                await transaction.RollbackAsync();
+
+                System.Console.WriteLine($"Something went wrong. Exiting without saving any seed data...");
+                throw;
+            }
+        }
+
+        private static async Task SeedRolesAsync(bool force, AuthDbSeedDataOptions seedDataOptions, AuthDbContext authDbContext)
+        {
+            if (await authDbContext.Roles.AnyAsync() && !force)
+            {
+                System.Console.WriteLine("There are already some roles in the DB. Skipping adding roles... (Use --force or -f flag if there is any new role in the seed data options).");
+                return;
+            }
+
+            int counter = 0;
+            foreach (var role in seedDataOptions.Roles)
+            {
+                if (force && await authDbContext.Roles.AnyAsync(r => r.Name == role))
+                {
+                    System.Console.WriteLine($"Role {role} is already in the DB. Skipping...");
+                    continue;
+                }
+
+                await authDbContext.Roles.AddAsync(new Role(role));
+                ++counter;
+            }
+
+            await authDbContext.SaveChangesAsync();
+
+            System.Console.WriteLine($"{counter} role(s) are added to the DB.");
+        }
+
+        private static async Task SeedAccountsAsync(bool force, AuthDbSeedDataOptions seedDataOptions, AuthDbContext authDbContext)
+        {
+            if (await authDbContext.Accounts.AnyAsync() && !force)
+            {
+                System.Console.WriteLine("There are already some accounts in the DB. Skipping adding accounts... (Use --force or -f flag if there is any new account in the seed data options).");
+                return;
+            }
+
+            var counter = 0;
+            foreach (var accountDetails in seedDataOptions.Accounts)
+            {
+                if (force && await authDbContext.Accounts.AnyAsync(a => a.Email == accountDetails.Email))
+                {
+                    System.Console.WriteLine($"Account with the email {accountDetails.Email} is already in the DB. Skipping...");
+                    continue;
+                }
+
+                var newAccount = new Account(
+                    accountDetails.Email,
+                    "123",  // The hashed password is given as a random string, which means no password will match.
+                            // The email owner must use the reset password feature to start using their account.
+                    SupportedLanguages.DefaultLanguage);
+
+                if (accountDetails.Role != null)
+                {
+                    var role = await authDbContext.Roles.FirstOrDefaultAsync(r => r.Name == accountDetails.Role);
+                    if (role == null)
+                        throw new RoleNotFoundException(accountDetails.Role);
+
+                    newAccount.Roles.Add(role);
+                }
+
+                if (accountDetails.PreferredLanguage != null)
+                {
+                    if (SupportedLanguages.AllLanguages.Contains(accountDetails.PreferredLanguage))
+                    {
+                        newAccount.PreferredLanguage = accountDetails.PreferredLanguage;
+                    }
+                }
+
+                await authDbContext.Accounts.AddAsync(newAccount);
+                ++counter;
+            }
+
+            await authDbContext.SaveChangesAsync();
+
+            System.Console.WriteLine($"{counter} account(s) are added to the DB.");
+        }
+    }
+}
---
+++ src/InitializerServices/test/integration/AuthDb.Test.Integration.Initializer/ProgramTest.cs
@@ -0,0 +1,221 @@
+using System.Reflection;
+using AuthDb.Initializer;
+using AuthDb.Initializer.Options;
+using AuthDb.Test.Integration.Initializer.Collections;
+using AuthService.Domain.Entities;
+using AuthService.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Shared.Constants;
+using Shared.Test.Generators;
+using Shared.Test.Helpers.Fixtures;
+
+namespace AuthDb.Test.Integration.Initializer
+{
+    [Collection(nameof(PostgreSqlCollection))]
+    public class ProgramTest
+    {
+        private readonly PostgreSqlFixture _postgreSqlFixture;
+
+        public ProgramTest(PostgreSqlCollectionCluster collectionCluster)
+        {
+            _postgreSqlFixture = collectionCluster.PostgreSqlFixture;
+        }
+
+        [Theory]
+        [InlineData(true, true, SupportedLanguages.German)]
+        [InlineData(true, true, null)]
+        [InlineData(true, true, "abc")]
+        [InlineData(true, false, null)]
+        [InlineData(false, true, SupportedLanguages.German)]
+        [InlineData(false, true, null)]
+        [InlineData(false, true, "abc")]
+        [InlineData(false, false, null)]
+        public async Task InitializeAsync_WhenThereIsNoDataInDb_ShouldAddSeedDataToDb(bool force, bool hasRole, string? language)
+        {
+            // Arrange
+            await _postgreSqlFixture.DropDatabaseAsync<AuthDbContext>();
+
+            var seedRoleName = StringGenerator.GenerateAlphanumeric();
+            var seedAccountEmail = EmailGenerator.Generate();
+            var seedDataOptions = new AuthDbSeedDataOptions()
+            {
+                Roles = new List<string>() { seedRoleName },
+                Accounts = new List<AuthDbSeedDataOptions.AccountDetails>()
+                {
+                    new AuthDbSeedDataOptions.AccountDetails()
+                    {
+                        Email = seedAccountEmail,
+                        Role = hasRole ? seedRoleName : null,
+                        PreferredLanguage = language
+                    }
+                }
+            };
+
+            using var authDbContext = _postgreSqlFixture.CreateDbContext<AuthDbContext>();
+
+            // Act
+            await ProgramInitializeAsync(force, seedDataOptions, authDbContext);
+
+            // Assert
+            authDbContext.ChangeTracker.Clear();
+
+            var rolesFromDb = await authDbContext.Roles.ToListAsync();
+            var accountsFromDb = await authDbContext.Accounts.Include(a => a.Roles).ToListAsync();
+            Assert.Single(rolesFromDb);
+            Assert.Single(accountsFromDb);
+
+            Assert.Equal(seedRoleName, rolesFromDb[0].Name);
+            Assert.Equal(seedAccountEmail, accountsFromDb[0].Email);
+            
+            if (hasRole)
+            {
+                Assert.Equal(seedRoleName, accountsFromDb[0].Roles.ElementAt(0).Name);
+            }
+            else
+            {
+                Assert.Empty(accountsFromDb[0].Roles);
+            }
+
+            if (language == SupportedLanguages.German)
+            {
+                Assert.Equal(language, accountsFromDb[0].PreferredLanguage);
+            }
+            else
+            {
+                Assert.Equal(SupportedLanguages.DefaultLanguage, accountsFromDb[0].PreferredLanguage);
+            }
+        }
+
+        [Fact]
+        public async Task InitializeAsync_WhenThereIsDataInDbAndNoForceFlag_ShouldDoNothing()
+        {
+            // Arrange
+            await _postgreSqlFixture.ClearDatabaseAsync<AuthDbContext>();
+
+            using var authDbContext = _postgreSqlFixture.CreateDbContext<AuthDbContext>();
+            await authDbContext.Roles.AddAsync(new Role(StringGenerator.GenerateAlphanumeric()));
+            await authDbContext.Accounts.AddAsync(new Account(EmailGenerator.Generate(), StringGenerator.GenerateAlphanumeric(), "abc"));
+            await authDbContext.SaveChangesAsync();
+
+            var seedRoleName = StringGenerator.GenerateAlphanumeric();
+            var seedAccountEmail = EmailGenerator.Generate();
+            var seedDataOptions = new AuthDbSeedDataOptions()
+            {
+                Roles = new List<string>() { seedRoleName },
+                Accounts = new List<AuthDbSeedDataOptions.AccountDetails>()
+                {
+                    new AuthDbSeedDataOptions.AccountDetails()
+                    {
+                        Email = seedAccountEmail
+                    }
+                }
+            };
+
+            // Act
+            await ProgramInitializeAsync(false, seedDataOptions, authDbContext);
+
+            // Assert
+            authDbContext.ChangeTracker.Clear();
+
+            var rolesFromDb = await authDbContext.Roles.ToListAsync();
+            var accountsFromDb = await authDbContext.Accounts.Include(a => a.Roles).ToListAsync();
+            Assert.Single(rolesFromDb);
+            Assert.Single(accountsFromDb);
+        }
+
+        [Fact]
+        public async Task InitializeAsync_WhenThereIsDataInDbAndForceFlagAndNoUniqueSeedData_ShouldDoNothing()
+        {
+            // Arrange
+            await _postgreSqlFixture.ClearDatabaseAsync<AuthDbContext>();
+
+            var seedRoleName = StringGenerator.GenerateAlphanumeric();
+            var seedAccountEmail = EmailGenerator.Generate();
+            var seedDataOptions = new AuthDbSeedDataOptions()
+            {
+                Roles = new List<string>() { seedRoleName },
+                Accounts = new List<AuthDbSeedDataOptions.AccountDetails>()
+                {
+                    new AuthDbSeedDataOptions.AccountDetails()
+                    {
+                        Email = seedAccountEmail
+                    }
+                }
+            };
+
+            using var authDbContext = _postgreSqlFixture.CreateDbContext<AuthDbContext>();
+            await authDbContext.Roles.AddAsync(new Role(seedRoleName));
+            await authDbContext.Accounts.AddAsync(new Account(seedAccountEmail, StringGenerator.GenerateAlphanumeric(), "abc"));
+            await authDbContext.SaveChangesAsync();
+
+            // Act
+            await ProgramInitializeAsync(true, seedDataOptions, authDbContext);
+
+            // Assert
+            authDbContext.ChangeTracker.Clear();
+
+            var rolesFromDb = await authDbContext.Roles.ToListAsync();
+            var accountsFromDb = await authDbContext.Accounts.Include(a => a.Roles).ToListAsync();
+            Assert.Single(rolesFromDb);
+            Assert.Single(accountsFromDb);
+        }
+
+        [Fact]
+        public async Task InitializeAsync_WhenThereIsDataInDbAndForceFlagAndUniqueSeedData_ShouldAddUniqueDataToDb()
+        {
+            // Arrange
+            await _postgreSqlFixture.ClearDatabaseAsync<AuthDbContext>();
+
+            var seedRoleName = StringGenerator.GenerateAlphanumeric();
+            var seedAccountEmail = EmailGenerator.Generate();
+            var seedDataOptions = new AuthDbSeedDataOptions()
+            {
+                Roles = new List<string>()
+                {
+                    seedRoleName,
+                    seedRoleName + "a"
+                },
+                Accounts = new List<AuthDbSeedDataOptions.AccountDetails>()
+                {
+                    new AuthDbSeedDataOptions.AccountDetails()
+                    {
+                        Email = seedAccountEmail
+                    },
+                    new AuthDbSeedDataOptions.AccountDetails()
+                    {
+                        Email = seedAccountEmail + "a"
+                    }
+                }
+            };
+
+            using var authDbContext = _postgreSqlFixture.CreateDbContext<AuthDbContext>();
+            await authDbContext.Roles.AddAsync(new Role(seedRoleName));
+            await authDbContext.Accounts.AddAsync(new Account(seedAccountEmail, StringGenerator.GenerateAlphanumeric(), "abc"));
+            await authDbContext.SaveChangesAsync();
+
+            // Act
+            await ProgramInitializeAsync(true, seedDataOptions, authDbContext);
+
+            // Assert
+            authDbContext.ChangeTracker.Clear();
+
+            var rolesFromDb = await authDbContext.Roles.ToListAsync();
+            var accountsFromDb = await authDbContext.Accounts.Include(a => a.Roles).ToListAsync();
+            Assert.Equal(2, rolesFromDb.Count);
+            Assert.Equal(2, accountsFromDb.Count);
+
+            Assert.Contains(seedRoleName + "a", rolesFromDb.Select(r => r.Name).ToList());
+
+            var account = accountsFromDb.FirstOrDefault(a => a.Email == seedAccountEmail + "a");
+            Assert.NotNull(account);
+            Assert.Empty(account.Roles);
+            Assert.Equal(SupportedLanguages.DefaultLanguage, account.PreferredLanguage);
+        }
+
+        private Task ProgramInitializeAsync(bool force, AuthDbSeedDataOptions seedDataOptions, AuthDbContext authDbContext)
+        {
+            var initializeMethodInfo = typeof(Program).GetMethod("InitializeAsync", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance)!;
+            return (Task)initializeMethodInfo.Invoke(null, [force, seedDataOptions, authDbContext])!;
+        }
+    }
+}
