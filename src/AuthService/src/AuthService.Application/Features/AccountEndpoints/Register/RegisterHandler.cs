85314837d4299b1d346a1fe698f89a44644a489f
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-09-14 21:59:24 +0200

    update endpoint results accordingly

--- src/AuthService/src/AuthService.Application/Features/AccountEndpoints/Register/RegisterHandler.cs
+++ src/AuthService/src/AuthService.Application/Features/AccountEndpoints/Register/RegisterHandler.cs
@@ -68,7 +68,7 @@ namespace AuthService.Application.Features.AccountEndpoints.Register
                 BodyParameters: [emailVerificationTokenValue])
             );
 
-            return JsonResponseBuilder.Success(HttpStatusCode.OK);
+            return JsonResponseBuilder.Success(HttpStatusCode.NoContent);
         }
     }
 }
