96c10eba2721af9c20e9c93c3dccb96e1e2568d0
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-09-15 00:58:54 +0200

    create test cases for get sessions endpoint

---
+++ src/AuthService/test/integration/AuthService.Test.Integration.Application/Features/AccountEndpoints/GetSessions/GetSessionsHandlerTest.cs
@@ -0,0 +1,86 @@
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using AuthService.Domain.Entities;
+using AuthService.Persistence.DbContexts;
+using AuthService.Test.Integration.Application.Collections;
+using Microsoft.EntityFrameworkCore;
+using Shared.Constants;
+using Shared.Http.Authentication;
+using Shared.Http.Response;
+using Shared.Test.Generators;
+
+namespace AuthService.Test.Integration.Application.Features.AccountEndpoints.GetSessions
+{
+    [Collection(nameof(AuthApiCollection))]
+    public class GetSessionsHandlerTest
+    {
+        private const string _endpoint = "/accounts/sessions";
+        
+        private readonly AuthApiCollectionCluster _collectionCluster;
+
+        public GetSessionsHandlerTest(AuthApiCollectionCluster collectionCluster)
+        {
+            _collectionCluster = collectionCluster;
+        }
+
+        [Fact]
+        public async Task Handle_WhenNotAuthenticated_ShouldReturnNotAuthorized()
+        {
+            // Arrange
+            var client = _collectionCluster.AuthApiWebApp.CreateHttpClient();
+
+            // Act
+            var response = await client.GetAsync(_endpoint);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Handle_WhenAuthenticatedAndThereAreSessions_ShouldReturnOkWithNotExpiredSessions()
+        {
+            // Arrange
+            await using var authDbContext = _collectionCluster.PostgreSqlFixture.CreateDbContext<AuthDbContext>();
+
+            var numberOfSessions = 3;
+            var account = new Account(EmailGenerator.Generate(), StringGenerator.GenerateAlphanumeric(), SupportedLanguages.DefaultLanguage);
+            for (int i = 0; i < numberOfSessions; ++i)
+            {
+                account.Sessions.Add(new Session(account.Id, StringGenerator.GenerateAlphanumeric(), DateTimeOffset.UtcNow.AddDays(1)));
+            }
+            account.Sessions.Add(new Session(account.Id, StringGenerator.GenerateAlphanumeric(), DateTimeOffset.UtcNow.AddDays(-1)));
+
+            await authDbContext.Accounts.AddAsync(account);
+            await authDbContext.SaveChangesAsync();
+            
+            var client = _collectionCluster.AuthApiWebApp.CreateHttpClient();
+            client.DefaultRequestHeaders.Add(ApiGatewayAuthKeys.Claims.Id.HeaderKey, account.Id.ToString());
+            client.DefaultRequestHeaders.Add(ApiGatewayAuthKeys.Claims.PreferredLanguage.HeaderKey, SupportedLanguages.DefaultLanguage);
+            client.DefaultRequestHeaders.Add(ApiGatewayAuthKeys.Claims.IssuedAt.HeaderKey, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString());
+
+            // Act
+            var response = await client.GetAsync(_endpoint);
+            var dto = await response.Content.ReadFromJsonAsync<Dto>();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            Assert.NotNull(dto);
+            Assert.Equal(numberOfSessions, dto.Data.Count);
+        }
+
+        private class Dto
+        {
+            public List<SessionDto> Data { get; set; } = new List<SessionDto>();
+
+            public class SessionDto
+            {
+                public DateTimeOffset CreatedDate { get; set; }
+                public DateTimeOffset? UpdatedDate { get; set; }
+                public IPAddress? IpAddress { get; set; }
+                public string? DeviceInfo { get; set; }
+            }
+        }
+    }
+}
