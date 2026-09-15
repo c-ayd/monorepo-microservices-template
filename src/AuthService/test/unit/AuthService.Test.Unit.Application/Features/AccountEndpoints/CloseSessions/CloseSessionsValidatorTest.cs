bc6c48b38287d8ab6199f1e502eb24d00dc64d55
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-09-15 11:24:18 +0200

    create test cases for close sessions validator

---
+++ src/AuthService/test/unit/AuthService.Test.Unit.Application/Features/AccountEndpoints/CloseSessions/CloseSessionsValidatorTest.cs
@@ -0,0 +1,41 @@
+using AuthService.Application.Features.AccountEndpoints.CloseSession;
+using Shared.Test.Generators;
+
+namespace AuthService.Test.Unit.Application.Features.AccountEndpoints.CloseSessions
+{
+    public class CloseSessionsValidatorTest
+    {
+        private readonly CloseSessionValidator _validator;
+
+        public CloseSessionsValidatorTest()
+        {
+            _validator = new CloseSessionValidator();
+        }
+
+        [Fact]
+        public void Validate_WhenPasswordIsNull_ShouldReturnError()
+        {
+            // Arrange
+            var request = new CloseSessionRequest(null);
+
+            // Act
+            var errors = _validator.Validate(request);
+
+            // Assert
+            Assert.Single(errors);
+        }
+
+        [Fact]
+        public void Validate_WhenPasswordIsValid_ShouldReturnNoError()
+        {
+            // Arrange
+            var request = new CloseSessionRequest(PasswordGenerator.GenerateValid());
+
+            // Act
+            var errors = _validator.Validate(request);
+
+            // Assert
+            Assert.Empty(errors);
+        }
+    }
+}
